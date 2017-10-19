b15691ae57005a7ca96efa7060b7ca29faf653be
cliffcole <clifflcole@gmail.com>
2017-10-19 09:48:29 -0400

    adding exercise6.js

--- exercise6.js
+++ exercise6.js
@@ -1,11 +1,23 @@
 const fileModule = require('./filemodule');
+const filter = require('./file_filter');
 
-fileModule.getFileExt(process.argv[2], process.argv[3], function(err,data){
+var callback = function(err,data){
+    
     if(err){
         throw err;
     }
     else {
         console.log(data);
     }
+}
 
+fileModule(process.argv[2], process.argv[3], function(err,data){
+    if(err){
+        console.log(err);
+    }
+    else{
+        data.forEach(function(element) {
+            console.log(element);
+        });
+    }
 });
--- filemodule.js
+++ filemodule.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.getFileExt = function(directory, extention, callback){
+
+module.exports = function(directory, extention, callback) {
     var returnData = [];
-    fs.readdir(directory, function(err, data){
+    fs.readdir(directory, (err, data) => {
         if(err) return callback(err);
         for(var x = 0; x < data.length; x++){
             var fileExt = path.extname(data[x])
-            if (fileExt == "."+extention){
-                returnData.push(data[x]);
+            
+            if (fileExt == "." + extention){
+                returnData.push(data[x]);   
             }
         }
+        return callback(null,returnData);  
     })
-    return callback(returnData);
 }
-
-module.exports = getFileExt;
