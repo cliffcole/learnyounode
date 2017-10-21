e0a93a4f6d1aa59fbd1f79f39458c46435ce9e7f
cliffcole <clifflcole@gmail.com>
2017-10-21 09:43:02 -0400

    adding exercise12

--- exercise10.js
+++ exercise10.js
@@ -4,13 +4,18 @@ var getDate = () => {
     let date = new Date();
     formatedMonth = date.getMonth() + 1;
     formatedDay = date.getDate();
+    formatedHours = date.getHours();
     if(formatedMonth.toString().split('').length < 2){
         formatedMonth = "0"+formatedMonth;
     }
     if(formatedDay.toString().split('').length < 2){
         formatedDay = "0"+formatedDay;
     }
-    let formatedDate = date.getFullYear() +"-"+formatedMonth + "-"+formatedDay + " "+date.getHours() +":"+date.getMinutes(); 
+    if(formatedHours.toString().split('').length < 2){
+        formatedHours = "0"+formatedHours;
+    }
+
+    let formatedDate = date.getFullYear() +"-"+formatedMonth + "-"+formatedDay + " "+formatedHours +":"+date.getMinutes(); 
     return formatedDate;
 }
 var server = net.createServer((socket) => {
--- exercise12.js
+++ exercise12.js
@@ -1,17 +1,22 @@
 const http = require('http');
+const qs = require('querystring');
 
 var port = process.argv[2];
 
 const server = http.createServer((request, response) => {
     if (request.method == "POST"){
-       // console.log(request);
-        var responseData = ""
+       //console.log(request);
+        var responseData = "";
+        
         request.on('data', (chunk) => {
-            responseData += chunk;
+            //console.log(chunk.toString());
+            responseData += chunk.toString().toUpperCase();
         })
        
         request.on('end', () => {
-            response.end("WORD");
+            //var post = qs.parse(responseData);
+            //console.log(post.toString().toUpperCase());
+            response.end(responseData);
         })  
     }  
 })
