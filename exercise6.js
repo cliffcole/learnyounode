f0b0a22470fb86426fd9125dcbd791617e69575a
cliffcole <clifflcole@gmail.com>
2017-10-23 23:02:29 -0400

    updating exercise6

--- exercise6.js
+++ exercise6.js
@@ -1,13 +1,15 @@
 const fileModule = require('./filemodule');
 
 
-fileModule(process.argv[2], process.argv[3], function(err,data){
+function printFiles(err,files){
     if(err){
         console.log(err);
     }
     else{
-        data.forEach(function(element) {
+        files.forEach(function(element) {
             console.log(element);
         });
     }
-});
+}
+
+fileModule(process.argv[2], process.argv[3], printFiles)
